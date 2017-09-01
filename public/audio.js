07b7bdfcff71eee89363e0e8b67ef080148a204f
Pete <peternatewood@gmail.com>
2017-09-01 18:10:45 -0400

    Add function to play radar ping sound

--- public/audio.js
+++ public/audio.js
@@ -1,1 +1,16 @@
 var audio = new AudioContext || new webkitAudioContext;
+
+function playRadarPing() {
+  var osc = audio.createOscillator();
+  var gain = audio.createGain();
+  var time = audio.currentTime;
+
+  gain.connect(audio.destination);
+  osc.connect(gain);
+
+  osc.type = 'triangle';
+  gain.gain.setValueAtTime(0.2, time);
+  gain.gain.linearRampToValueAtTime(0, time + 0.2);
+  osc.start(time);
+  osc.stop(time + 0.2);
+}
