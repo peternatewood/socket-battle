840073e325ce069439670dd0e535aa6dc15cb2ea
Peter Wood <pwood@sankyinc.com>
2017-09-08 11:49:15 -0400

    Add function to play splash sound

--- public/audio.js
+++ public/audio.js
@@ -132,3 +132,45 @@ function playShipHitSound(audio) {
   osc.start(time);
   osc.stop(time + 0.71);
 }
+
+function playSplashSound(audio) {
+  // In case webAudio is not supported
+  if (! audio) {
+    return;
+  }
+
+  var gain = audio.createGain();
+  var time = audio.currentTime;
+
+  var pink = (function() {
+    var bufferSize = 4096;
+    var b0, b1, b2, b3, b4, b5, b6;
+    b0 = b1 = b2 = b3 = b4 = b5 = b6 = 0.0;
+    var node = audio.createScriptProcessor(bufferSize, 1, 1);
+    node.onaudioprocess = function(e) {
+      var output = e.outputBuffer.getChannelData(0);
+      for (var i = 0; i < bufferSize; i++) {
+        var white = Math.random() * 2 - 1;
+        b0 = 0.99886 * b0 + white * 0.0555179;
+        b1 = 0.99332 * b1 + white * 0.0750759;
+        b2 = 0.96900 * b2 + white * 0.1538520;
+        b3 = 0.86650 * b3 + white * 0.3104856;
+        b4 = 0.55000 * b4 + white * 0.5329522;
+        b5 = -0.7616 * b5 - white * 0.0168980;
+        output[i] = b0 + b1 + b2 + b3 + b4 + b5 + b6 + white * 0.5362;
+        output[i] *= 0.11; // (roughly) compensate for gain
+        b6 = white * 0.115926;
+      }
+    }
+    return node;
+  })();
+
+  gain.gain.setValueAtTime(0, time);
+  gain.gain.linearRampToValueAtTime(0.3, time + 0.01);
+  gain.gain.linearRampToValueAtTime(0, time + 0.8);
+
+  gain.connect(audio.destination);
+  pink.connect(gain);
+
+  setTimeout(function() { pink = null; }, 1000);
+}
