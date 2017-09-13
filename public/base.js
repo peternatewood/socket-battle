5f2baac5cb082e96737934ed2aaeb0fa7cd9cd60
Peter Wood <pwood@sankyinc.com>
2017-09-13 12:49:37 -0400

    Remove redundant ship tile assignment in updateBoard

--- public/base.js
+++ public/base.js
@@ -58,20 +58,14 @@ function clearTiles(board, ship) {
 
 // Returns true only if the ship is overlaying all empty tiles
 function updateBoard(board, ship) {
-  var bounds = ship.getBounds();
-  var increment = ship.rad == 0 || ship.rad == PI ? 1 : 12;
-  var shipHead = (bounds.l - 40) / 40 + 12 * (bounds.t - 80) / 40;
-  var shipTail = shipHead + increment * ship.size;
-
   var tiles = [];
-  for (var i = shipHead; i < shipTail; i += increment) {
-    // If tile is occupied by a ship
-    if (board[i]) {
+  for (var i = 0; i < ship.size; i++) {
+    // If tile is occupied by a ship or ship is not on the board
+    if (typeof ship.tiles[i] != 'number' || board[ship.tiles[i]]) {
       return false;
     }
     tiles.push(i);
   }
-  ship.tiles = tiles;
 
   for (var i = 0; i < ship.size; i++) {
     board[tiles[i]] = 1;
