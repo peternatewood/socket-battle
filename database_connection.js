e73bd0a93417e2e43a912e30fb8742dca86c4041
Peter Wood <pwood@sankyinc.com>
2017-09-14 13:11:25 -0400

    Add postgres connection class in database_connection library

--- database_connection.js
+++ database_connection.js
@@ -64,12 +64,65 @@ mysqlConnection.prototype.signoutUser = function(username, callback, errCallback
   });
 };
 
+var psqlConnection = function(credentials) {
+  var pgp = require('pg-promise')();
+
+  this.connection = pgp(credentials);
+  return this;
+}
+psqlConnection.prototype.getUsers = function(callback, errCallback) {
+  var query = this.connection.any('SELECT username, password_hash, token FROM users');
+
+  if (errCallback) {
+    query.catch(errCallback);
+  }
+
+  query.then(callback);
+}
+psqlConnection.prototype.createUser = function(fields, callback, errCallback) {
+  var fieldsList = [
+    fields.username,
+    fields.password_hash,
+    fields.token,
+    fields.created_at,
+    fields.updated_at
+  ];
+
+  var query = this.connection.none('INSERT INTO users(username, password_hash, token, created_at, updated_at) VALUES($1, $2, $3, $4, $5)', fieldsList);
+
+  if (errCallback) {
+    query.catch(errCallback);
+  }
+
+  query.then(callback);
+}
+psqlConnection.prototype.loginUser = function(token, username, callback, errCallback) {
+  var query = this.connection.none('UPDATE users SET token = $1 WHERE username = $2', [token, username]);
+
+  if (errCallback) {
+    query.catch(errCallback);
+  }
+
+  query.then(callback);
+}
+psqlConnection.prototype.signoutUser = function(username, callback, errCallback) {
+  var query = this.connection.none('UPDATE users SET token = NULL WHERE username = $1', username);
+
+  if (errCallback) {
+    query.catch(errCallback);
+  }
+
+  query.then(callback);
+}
+
 module.exports = (credentials, type) => {
   switch (type) {
     case 'mysql':
       return new mysqlConnection(credentials);
       break;
-    default:
-      return new mysqlConnection(credentials);
+    case 'psql':
+    case 'postgres':
+      return new psqlConnection(credentials);
+      break;
   }
 };
