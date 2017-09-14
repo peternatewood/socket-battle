6ba11ea3c71b70ad2a4b208d765e7521501585b6
Peter Wood <pwood@sankyinc.com>
2017-09-14 17:07:16 -0400

    Add prerender script and title canvas

---
+++ public/prerender.js
@@ -0,0 +1,9 @@
+ready(function() {
+  var title = document.getElementById('title').getContext('2d');
+
+  title.fillStyle = '#331';
+  title.font = '120px Black Ops One';
+  title.textAlign = 'left';
+  title.textBaseline = 'top';
+  title.fillText('Sea Battle', -7, 0);
+});
